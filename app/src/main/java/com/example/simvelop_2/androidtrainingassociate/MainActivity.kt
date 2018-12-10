476b099b1c96ee18e310f2f44fdb3df8175b40d6
Laert <laert@simvelop.de>
2018-12-10 15:20:18 +0100

    changes

--- app/src/main/java/com/example/simvelop_2/androidtrainingassociate/MainActivity.kt
+++ app/src/main/java/com/example/simvelop_2/androidtrainingassociate/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.simvelop_2.androidtrainingassociate
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,4 +12,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onStart() {
+        super.onStart()
+        show_count?.text = 0.toString()
+    }
+
+    fun btnShowToast_Click(view: View){
+        Toast.makeText(this, getString(R.string.toast_message),Toast.LENGTH_SHORT).show()
+    }
+
+    private var mCount = 0
+
+    fun btnCountUp_Click(view: View){
+        mCount++
+        show_count.text = mCount.toString()
+    }
+
+
+
+
 }
